9f858aae3b9477c72801cdce7617ddd2d633bc45
Chanatinart <gift_nk_61@hotmail.com>
2022-07-26 01:00:59 +0700

    Try with If fuction still not work

--- javascript.js
+++ javascript.js
@@ -3,9 +3,79 @@ const keys = document.querySelector('.calculator_keys')
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) {
-        //do something
+        const key = e.target
+        const action = key.dataset.action
+
+        if(
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key')
+        } 
+        if(!action) {
+            console.log('number key')
+        }
+        if(action === 'decimal') {
+            console.log('decimal key')
+        }
+        if(action === 'clear') {
+            console.log('clear key')
+        }
+        if(action === 'calculate') {
+            console.log('equal key')
+        }
+        if(action === 'delete') {
+            console.log('delete key')
+        }
+        if(action === 'positive-negative') {
+            console.log('ps key')
+        }
     }
 })
 
-const key = e.target
-const action = key.dataset.action
+
+const display = document.querySelector('.calculator_display')
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayNum = display.textContent
+
+    if(!action) {
+        if (displayNum === '0') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayNum + keyContent //append the clicked num to display num
+            }
+        }
+        //click the decimal
+    if(action === 'decimal') {
+            display.textContent = displayNum + '.'
+        }
+    // click an operator key
+    if(
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide' 
+    ) {
+        key.classList.add('is-depressed')
+    }
+
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        if(!action) {
+            if(displayNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent 
+            } else {
+                display.textContent = displayNum + keyContent
+            }
+        calculator.dataset.previousKeyType = 'operator'
+        }
+    }
+})
+
