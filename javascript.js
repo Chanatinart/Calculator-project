cfa16ab806f0544adeb809d65e394c48c5cc2326
Chanatinart <gift_nk_61@hotmail.com>
2022-07-26 17:11:47 +0700

    still not working

--- javascript.js
+++ javascript.js
@@ -1,33 +1,95 @@
-const calculator = document.querySelector('.calculator')
-const keys = document.querySelector('.calculator_keys')
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator_keys');
+const userInput = document.querySelector('#user-input');
+const displayResult = document.querySelector('#result');
+const previousKeyType = calculator.dataset.previousKeyType
+
+let firstValue = ''
+
 
 keys.addEventListener('click', e => {
     if(e.target.matches('button')) {
-        const key = e.target
-        const action = key.dataset.action
-
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayNum = userInput.textContent
         if(
             action === 'add' ||
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
         ) {
-            console.log('operator key')
+             const firstValue = calculator.dataset.firstValue
+             const operator = calculator.dataset.operator
+             const secondValue = displayNum
+
+             if (firstValue && operator) {
+                userInput.textContent = calculator(firstValue, operator, secondValue)
+  //                          firstValue = userInput.textContent
+    //        userInput.textContent = keyContent
+        //     displayResult.textContent = `${firstValue} ${keyContent}`
+
+           calculator.dataset.previousKeyType = 'operator'
+
+           calculator.dataset.firstValue = displayNum
+
+           calculator.dataset.operator = action
+             }
+
+
         } 
         if(!action) {
-            console.log('number key')
+                if (displayNum === '0'|| 
+                    previousKeyType === 'operator') {
+                userInput.textContent = keyContent
+            } else {
+                userInput.textContent = displayNum + keyContent //append the clicked num to display num
+
+                calculator.dataset.previousKeyType = 'number'
+            }
         }
         if(action === 'decimal') {
-            console.log('decimal key')
+            if(!displayNum.includes('.')) {
+                userInput.textContent = displayNum + '.'   
+            } else if(previousKeyType === 'operator') {
+                userInput.textContent = '0.'
+            }
+            
+            calculator.dataset.previousKeyType = 'decimal'
         }
         if(action === 'clear') {
-            console.log('clear key')
+            userInput.textContent =  '0'
+            displayResult.textContent = ''
+            calculator.dataset.previousKeyType = 'clear'
+
         }
         if(action === 'calculate') {
-            console.log('equal key')
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayNum
+
+            const calculate = (n1, operator, n2) => {
+                let result = ''
+
+                if(operator === 'add') {
+                    result = parseFloat(n1) + parseFloat(n2)
+                } else if(operator === 'subtract') {
+                    result = parseFloat(n1) - parseFloat(n2)
+                } else if(operator === 'multiply') {
+                    result = parseFloat(n1) * parseFloat(n2)
+                } else if(operator === 'divide') {
+                    result = parseFloat(n1) / parseFloat(n2)
+                }
+                return result
+            calculator.dataset.previousKeyType = 'calculate'
+            }
+
+            userInput.textContent = calculate(firstValue, operator, secondValue)
         }
         if(action === 'delete') {
-            console.log('delete key')
+            userInput.textContent = userInput.textContent
+            .toString()
+            .slice(0, -1)
         }
         if(action === 'positive-negative') {
             console.log('ps key')
@@ -35,47 +97,6 @@ keys.addEventListener('click', e => {
     }
 })
 
-
-const display = document.querySelector('.calculator_display')
-
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent
-        const displayNum = display.textContent
-
-    if(!action) {
-        if (displayNum === '0') {
-                display.textContent = keyContent
-            } else {
-                display.textContent = displayNum + keyContent //append the clicked num to display num
-            }
-        }
-        //click the decimal
-    if(action === 'decimal') {
-            display.textContent = displayNum + '.'
-        }
-    // click an operator key
-    if(
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide' 
-    ) {
-        key.classList.add('is-depressed')
-    }
-
-        const previousKeyType = calculator.dataset.previousKeyType
-
-        if(!action) {
-            if(displayNum === '0' || previousKeyType === 'operator') {
-                display.textContent = keyContent 
-            } else {
-                display.textContent = displayNum + keyContent
-            }
-        calculator.dataset.previousKeyType = 'operator'
-        }
-    }
-})
-
+ //
+   //         displayResult.textContent = `${firstValue}${userInput}`
+     //       
